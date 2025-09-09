d529ed2f39d9e6bdd1b700c04de6e0a8d4b2c03a
seroking <baridmohamedamine004@gmail.com>
2025-09-09 23:34:40 +0100

    first commit

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SignUp() {
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+    </>
+  )
+}
---
+++ app/tool/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Tool() {
+
+  return (
+    <>
+      <h1>Tool Page</h1>
+    </>
+  )
+}
